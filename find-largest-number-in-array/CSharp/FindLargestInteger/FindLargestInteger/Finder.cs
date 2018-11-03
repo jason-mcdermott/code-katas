cb6ba4fe5c3ad93072a34687fe937480cced5249
Jason McDermott <jmcdermott@rightpoint.com>
2018-11-03 09:55:31 -0500

    c# solution

---
+++ find-largest-number-in-array/CSharp/FindLargestInteger/FindLargestInteger/Finder.cs
@@ -0,0 +1,32 @@
+﻿
+namespace FindLargestInteger
+{
+    public class Finder
+    {
+        public int FindLargest(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i + 1 < numbers.Length)
+                {
+                    var next = numbers[i + 1];
+                    var current = numbers[i];
+                    if (current > next)
+                    {
+                        Swap(i, numbers);
+                    }
+                }
+            }
+
+            return numbers[numbers.Length-1];
+        }
+
+        private void Swap(int index, int[] array)
+        {
+            var next = array[index + 1];
+            var current = array[index];
+            array[index] = next;
+            array[index + 1] = current;
+        }
+    }
+}
