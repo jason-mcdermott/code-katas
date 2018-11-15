b332054450b7c74603ffcd1999c7db70a5dc3d8c
Jason McDermott <jmcdermott@rightpoint.com>
2018-11-14 18:06:40 -0600

    c# solution

---
+++ selection-sort/CSharp/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new[] { 12, 2, 7, 4, 5, 6, 3, 8, 1, 0, 9, 10, 11 };
+            
+            var sortedItems = SelectionSort.Sort(items);
+
+            foreach (var item in sortedItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ selection-sort/CSharp/SelectionSort/SelectionSort/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿
+namespace SelectionSort
+{
+    public static class SelectionSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                var currentMinIndex = i;
+                for (var j = currentMinIndex; j < array.Length; j++)
+                {
+                    if (array[j] < array[currentMinIndex])
+                    {
+                        currentMinIndex = j;
+                    }
+                }
+
+                if (currentMinIndex != i)
+                {
+                    var originalMinValue = array[i];
+                    array[i] = array[currentMinIndex];
+                    array[currentMinIndex] = originalMinValue;
+                }
+            }
+
+            return array;
+        }
+    }
+}
