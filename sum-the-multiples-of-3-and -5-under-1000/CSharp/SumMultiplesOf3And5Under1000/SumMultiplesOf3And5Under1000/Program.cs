cd19cbd0b676c7b354e8e25c4e46b77e3e7a6389
Jason McDermott <jmcdermott@rightpoint.com>
2018-11-27 18:41:24 -0600

    c# solutions

---
+++ sum-the-multiples-of-3-and -5-under-1000/CSharp/SumMultiplesOf3And5Under1000/SumMultiplesOf3And5Under1000/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumMultiplesOf3And5Under1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Brute-force: {SumMultiples.BruteForceSum()}");
+            Console.WriteLine($"Recursive: {SumMultiples.RecursiveSum(999, 0)}");
+            Console.WriteLine($"Optimized: {SumMultiples.OptimizedSum()}");
+        }
+    }
+}
---
+++ sum-the-multiples-of-3-and -5-under-1000/CSharp/SumMultiplesOf3And5Under1000/SumMultiplesOf3And5Under1000/SumMultiples.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumMultiplesOf3And5Under1000
+{
+    public class SumMultiples
+    {
+        private static int counter = 0;
+        public static int BruteForceSum()
+        {
+            int sum = 0;
+           
+            for (var i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        public static int RecursiveSum(int x, int sum)
+        {
+            if (x <= 0)
+            {
+                return sum;
+            }
+
+            if (x % 3 == 0 || x % 5 == 0)
+            {
+                sum += x;
+            }
+            
+            return RecursiveSum(x - 1, sum);
+        }
+
+        public static int OptimizedSum()
+        {
+            var dict = new Dictionary<int, int>();
+            int sum = 0;
+           
+            for (var i = 999; i >= 3; i = i - 3)
+            {
+                // check to make sure value has not been included already
+                if (!dict.ContainsKey(i))
+                {
+                    dict.Add(i, i);
+                    sum += i;
+                }
+            }
+
+            for (var j = 995; j >= 5; j = j - 5)
+            {
+                // check to make sure value has not been included already
+                if (!dict.ContainsKey(j))
+                {
+                    dict.Add(j, j);
+                    sum += j;
+                }
+            }
+            
+            return sum;
+        }
+    }
+}
