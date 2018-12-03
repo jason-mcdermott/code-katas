5bfd1610611d12f1bf9083e354be45a681272ce2
Jason McDermott <jmcdermott@rightpoint.com>
2018-12-03 17:00:14 -0600

    c# solutions

---
+++ sum-even-values-of-fibonacci/CSharp/SumEvenValuesOfFibonacci/SumEvenValuesOfFibonacci/SumEvens.cs
@@ -0,0 +1,48 @@
+﻿
+namespace SumEvenValuesOfFibonacci
+{
+    public class SumEvens
+    {
+        private static int _max = 4000000;
+        public static int SumFibonacci(int max)
+        {
+            int a = 1;
+            int b = 2;
+            int sum = b;
+            int temp = 0;
+
+            while (a + b <= max)
+            {
+                temp = a + b;
+                a = b;
+                b = temp;
+
+                if (temp % 2 == 0)
+                {
+                    sum += temp;
+                }
+            }
+
+            return sum;
+        }
+
+        public static int RecursiveSumFibonacci(int a, int b, int sum)
+        {
+            if (a + b >= _max)
+            {
+                return sum;
+            }
+
+            int temp = a + b;
+            a = b;
+            b = temp;
+
+            if (temp % 2 == 0)
+            {
+                sum += temp;
+            }
+
+            return RecursiveSumFibonacci(a, b, sum);
+        }
+    }
+}
