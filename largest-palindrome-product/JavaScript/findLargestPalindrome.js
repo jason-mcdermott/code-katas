f1109feba52e27b58a23a5af99bae30f1b2021b9
Jason McDermott <jmcdermott@rightpoint.com>
2018-12-05 18:21:48 -0600

    fix comment

--- largest-palindrome-product/JavaScript/findLargestPalindrome.js
+++ largest-palindrome-product/JavaScript/findLargestPalindrome.js
@@ -1,4 +1,4 @@
-//  CHALLENGE (taken from Project Euler, no. 2):
+//  CHALLENGE (taken from Project Euler, no. 4):
 //  
 //  A palindromic number reads the same both ways. The largest palindrome made 
 //  from the product of two 2-digit numbers is 9009 = 91 × 99.
