da81936a8252663a73fe4b401eacc1761ab61b7f
Jason McDermott <jmcdermott@rightpoint.com>
2018-12-08 21:08:02 -0600

    c# solution

---
+++ largest-palindrome-product/CSharp/FindLargestPalindrome/FindLargestPalindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindLargestPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalindromeFinder.Find());
+        }
+    }
+}
