a81b9bd01957aadf1cfb24772a1df90b4834eca6
Jason McDermott <veloci.cello@gmail.com>
2019-01-15 20:20:12 -0600

    add test cases

--- get-index-to-insert/JavaScript/app.js
+++ get-index-to-insert/JavaScript/app.js
@@ -5,33 +5,36 @@
 //  returned value should be a number.
 
 function getIndexToIns(arr, num) {
-    arr.sort((x, y) => x - y);
-    let curr = 0;
-    let next = 1;
-  
-    if(num >= arr[0]){
-      while(next < arr.length){
-        if(num <= arr[curr]){
-          return curr;
-        }
-        if(num > arr[curr] && num <= arr[next]){
-          return next;
-        }
-        curr += 1;
-        next += 1;
+  arr.sort((x, y) => x - y);
+  let curr = 0;
+  let next = 1;
+
+  if(num >= arr[0]){
+    while(next < arr.length){
+      if(num <= arr[curr]){
+        return curr;
       }
-      // greater than last item
-      return arr.length;
+      if(num > arr[curr] && num <= arr[next]){
+        return next;
+      }
+      curr += 1;
+      next += 1;
     }
-    else {
-      // lower than first item
-      return 0;
-    }
+    // greater than last item
+    return arr.length;
   }
+  else {
+    // lower than first item
+    return 0;
+  }
+}
   
-  // let test = 
-  //   getIndexToIns([2, 20, 10], 19);
-  
-  // console.log(test);
-  console.log(getIndexToIns([5, 3, 20, 3], 5))// should return 2.
-  console.log(getIndexToIns([2, 20, 10], 19))// should return 2.
+console.log(getIndexToIns([5, 3, 20, 3], 5))  // should return 2.
+console.log(getIndexToIns([2, 20, 10], 19)) // should return 2.
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)) // should return 3.
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)) // should return 2.
+console.log(getIndexToIns([40, 60], 50)) //should return 1.
+console.log(getIndexToIns([3, 10, 5], 3)) //should return 0.
+console.log(getIndexToIns([5, 3, 20, 3], 5)) //should return 2.
+console.log(getIndexToIns([2, 20, 10], 19)) //should return 2.
+console.log(getIndexToIns([2, 5, 10], 15)) //should return 3.
