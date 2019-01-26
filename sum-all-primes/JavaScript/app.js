915992ce90241a089c77faceae85c2ca4141132c
Jason McDermott <veloci.cello@gmail.com>
2019-01-25 19:12:40 -0600

    remove unnecessary check

--- sum-all-primes/JavaScript/app.js
+++ sum-all-primes/JavaScript/app.js
@@ -13,26 +13,26 @@ function getPrimes(n) {
     let start = 2;
   
     for(let i = start; i <= n; i++){
-      primes.push(i);
+        primes.push(i);
     }
   
     let current = start;
     while(current <= n){
-      primes.forEach((p) => {
-        if(p > current && (p % current === 0 || p === (current * current))){
-          primes.splice(primes.indexOf(p), 1);
-        }
-      });
-      current = primes[primes.indexOf(current)+1];
+        primes.forEach((p) => {
+            if(p > current && p % current === 0){
+                primes.splice(primes.indexOf(p), 1);
+            }
+        });
+        current = primes[primes.indexOf(current)+1];
     }
   
     return primes;
-  }
+}
   
-  function sumPrimes(num) {
+function sumPrimes(num) {
     let primes = getPrimes(num);
     return primes.reduce((x, y) => x + y);
-  }
+}
 
 console.log(sumPrimes(10)); // should return a number.
 console.log(sumPrimes(10)); // should return 17.
